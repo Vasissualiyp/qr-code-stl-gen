24dd1b1ffa825456a952848efba636ba29efcdb6
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-06-21 11:03:07 -0400

    Formatting

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@ pkgs.mkShell {
     buildInputs = with pkgs; [ 
       blender
       f3d
-	  python311Packages.numpy 
+      python311Packages.numpy 
       python311Packages.pillow 
       python311Packages.trimesh 
       python311Packages.qrcode 
