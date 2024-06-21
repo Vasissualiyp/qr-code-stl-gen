a74f4e5ae918d9c343906efe26ff6ac3211ccffe
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-06-21 01:07:19 -0400

    Added gitignore

--- qrcodegen.py
+++ qrcodegen.py
@@ -1,23 +1,28 @@
 import qrcode
+import numpy as np
 from PIL import Image
-
 
 # taking image which user wants 
 # in the QR code center
-Logo_link = './images/cv_icon.png'
+Logo_link = './test.png'
  
 logo = Image.open(Logo_link)
  
 # taking base width
 basewidth = 100
  
-# adjust image size
-wpercent = (basewidth/float(logo.size[0]))
-hsize = int((float(logo.size[1])*float(wpercent)))
-logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
 QRcode = qrcode.QRCode(
     error_correction=qrcode.constants.ERROR_CORRECT_H
 )
+qr_matrix = QRcode.get_matrix()
+qr_matrix_size = len(qr_matrix)
+basewidth = qr_matrix_size
+
+# adjust image size
+#wpercent = (basewidth/float(logo.size[0]))
+#hsize = int((float(logo.size[1])*float(wpercent)))
+hsize = basewidth
+#logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
  
 # taking url or text
 url = 'https://www.zou-mse-utoronto-ca.net/'
@@ -33,15 +38,19 @@ QRcode.make()
  
 # adding color to QR code
 QRimg = QRcode.make_image( back_color="white").convert('RGB')
- 
+
 # set size of QR code
-pos = ((QRimg.size[0] - logo.size[0]) // 2,
-       (QRimg.size[1] - logo.size[1]) // 2)
-QRimg.paste(logo, pos)
- 
+logo_sizex = logo.size[0]
+logo_sizey = logo.size[1]
+pos = ((QRimg.size[0] - logo_sizex) // 2,
+       (QRimg.size[1] - logo_sizey) // 2)
+
+# Paste logo
+#QRimg.paste(logo, pos)
+
 # save the QR code generated
-QRimg.save('./output/xiao.png')
+QRimg.save('./qrout.png')
 qr_matrix = QRcode.get_matrix()
  
 print('QR code generated!')
-print(qr_matrix)
+print(len(qr_matrix))
