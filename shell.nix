a87266c1adbb87b4255decd8b881c6f3dda60d66
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-06-21 10:06:57 -0400

    Added a minimalist 3d viewer to the nix shell

--- shell.nix
+++ shell.nix
@@ -1,4 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
-    buildInputs = with pkgs.python311Packages; [ numpy pillow trimesh qrcode ];
+    buildInputs = with pkgs; [ 
+	  blender
+	  f3d
+	  python311Packages.numpy 
+      python311Packages.pillow 
+      python311Packages.trimesh 
+      python311Packages.qrcode 
+	];
 }
